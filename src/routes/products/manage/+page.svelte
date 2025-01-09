5f4b790a43378d69738dab29334c188db469f710
Chico <franciscommcastro@gmail.com>
2025-01-08 22:31:16 -0300

    feat: nav/backbuttons

--- src/routes/products/manage/+page.svelte
+++ src/routes/products/manage/+page.svelte
@@ -149,9 +149,21 @@
 <div class="py-10">
   <header>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">
-        Gerenciar Produtos
-      </h1>
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="flex-1 min-w-0">
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">
+            Gerenciar Produtos
+          </h1>
+        </div>
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+          <a
+            href="/products"
+            class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+          >
+            Voltar
+          </a>
+        </div>
+      </div>
     </div>
   </header>
   <main class="relative">
--- src/routes/purchases/+page.svelte
+++ src/routes/purchases/+page.svelte
@@ -35,9 +35,21 @@
 <div class="py-10">
   <header>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold leading-tight text-gray-900">
-        Compras
-      </h1>
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="flex-1 min-w-0">
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">
+            Compras
+          </h1>
+        </div>
+        <div class="mt-4 flex md:mt-0 md:ml-4">
+          <a
+            href="/"
+            class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Fazer upload
+          </a>
+        </div>
+      </div>
     </div>
   </header>
   <main>
